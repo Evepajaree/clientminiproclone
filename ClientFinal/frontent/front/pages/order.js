2def9e4de129622b65ec3dd63bd890221571cd9b
Evepajaree <47072684+Evepajaree@users.noreply.github.com>
2021-07-30 04:36:49 +0700

    Add files via upload

---
+++ ClientFinal/frontent/front/pages/order.js
@@ -0,0 +1,45 @@
+import Layout from '../components/layout'
+import Head from 'next/head'
+import config from '../config/config'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/navbar'
+
+const delivery = () => {
+    return (<Layout>
+        <Head>
+            <title>วิธีสั่งซื้อ</title>
+        </Head>
+        <div className={styles.container}>
+            <Navbar />
+            <h2> วิธีสั่งซื้อ </h2>
+            <b>Config: </b> {JSON.stringify(config)}
+            <ul>
+                <li>npm run dev  (for development mode)</li>
+                <li>npm run build; npm run start  (for production mode)</li>
+            </ul>
+            <div class="mb-3">
+  <label for="formFile" class="form-label">Default file input example</label>
+  <input class="form-control" type="file" id="formFile"/>
+</div>
+<div class="mb-3">
+  <label for="formFileMultiple" class="form-label">Multiple files input example</label>
+  <input class="form-control" type="file" id="formFileMultiple" multiple/>
+</div>
+<div class="mb-3">
+  <label for="formFileDisabled" class="form-label">Disabled file input example</label>
+  <input class="form-control" type="file" id="formFileDisabled" disabled/>
+</div>
+<div class="mb-3">
+  <label for="formFileSm" class="form-label">Small file input example</label>
+  <input class="form-control form-control-sm" id="formFileSm" type="file"/>
+</div>
+<div>
+  <label for="formFileLg" class="form-label">Large file input example</label>
+  <input class="form-control form-control-lg" id="formFileLg" type="file"/>
+</div>
+        </div>
+
+    </Layout>)
+}
+
+export default delivery
